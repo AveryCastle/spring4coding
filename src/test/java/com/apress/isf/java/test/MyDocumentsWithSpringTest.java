645e0f4729515cda64bb5fb2c9ac3cb39b92dae4
JuHyun Seong <ernestogevar@naver.com>
2015-10-28 21:36:49 +0900

    2장 2절 스프링프레임워크 사용하기 실습
    - 2장 2절 스프링프레임워크 사용하기 실습.
    - 예제 2-7, 2-8 실습.

---
+++ src/test/java/com/apress/isf/java/test/MyDocumentsWithSpringTest.java
@@ -0,0 +1,48 @@
+package com.apress.isf.java.test;
+
+import com.apress.isf.java.model.Document;
+import com.apress.isf.java.model.Type;
+import com.apress.isf.java.service.SearchEngine;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by seoungjuhyun on 10/28/15.
+ */
+public class MyDocumentsWithSpringTest {
+
+    private ClassPathXmlApplicationContext context;
+    private SearchEngine engine;
+    private Type documentType;
+
+    @Before
+    public void setup() {
+        context = new ClassPathXmlApplicationContext("META-INF/spring/mydocuments-context.xml");
+        engine = context.getBean(SearchEngine.class);
+        documentType = context.getBean(Type.class);
+    }
+
+    @Test
+    public void testWithSpringFindByType() {
+        List<Document> documents = engine.findByType(documentType);
+        assertNotNull(documents);
+        assertTrue(documents.size() == 2);
+        assertEquals(documentType.getName(), documents.get(0).getType().getName());
+        assertEquals(documentType.getDesc(), documents.get(0).getType().getDesc());
+        assertEquals(documentType.getExtension(), documents.get(0).getType().getExtension());
+    }
+
+    @Test
+    public void testWithSpringListAll() {
+        List<Document> documents = engine.listAll();
+        assertNotNull(documents);
+        assertTrue(documents.size() == 4);
+    }
+}
