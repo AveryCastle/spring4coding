644728795deebea9e12a9d6d85a5b657d6035fcb
JuHyun Seong <ernestogevar@naver.com>
2015-10-28 09:46:26 +0900

    2장 클래스와 의존성 다루기 - 01장 실습
    01. My Documents 스프링 애플리케이션 실습.
    예제 2-1 ~ 2-6까지 실습.

---
+++ src/main/java/com/apress/isf/java/model/Type.java
@@ -0,0 +1,35 @@
+package com.apress.isf.java.model;
+
+/**
+ * Created by seoungjuhyun on 10/28/15.
+ */
+public class Type {
+
+    private String name;
+    private String desc;
+    private String extension;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+}
--- src/main/java/com/apress/isf/spring/HelloWorldController.java
+++ src/main/java/com/apress/isf/spring/HelloWorldController.java
@@ -1,23 +1,19 @@
 package com.apress.isf.spring;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by seoungjuhyun on 10/27/15.
  */
-@RestController
-@EnableAutoConfiguration
+//@RestController
+//@EnableAutoConfiguration
 public class HelloWorldController {
 
-    @RequestMapping("/")
-    public String getMessage(){
-        return "<h1>Hello, World</h1>";
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(HelloWorldController.class, args);
-    }
+//    @RequestMapping("/")
+//    public String getMessage(){
+//        return "<h1>Hello, World</h1>";
+//    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(HelloWorldController.class, args);
+//    }
 }
